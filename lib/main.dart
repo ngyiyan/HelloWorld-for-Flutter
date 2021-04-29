8481f13db5ffa3f2d2649cb78679582774664b21
ngyiyan <31990645+ngyiyan@users.noreply.github.com>
2021-04-29 23:11:35 +0800

    Update main.dart
    Include AppBar shown "My First Project - Hello World!"

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,30 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      decoration: new BoxDecoration(color: Colors.white),
-      child: new Center(
-        child: new Text(
-          "Hello, World!",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            color: Colors.black,
+    return MaterialApp(
+      title: 'Hello World to Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("My First Project - Hello World!")),
+      body: Container(
+        //decoration: new BoxDecoration(color: Colors.white),
+        child: new Center(
+          child: new Text(
+            "Hello, World!",
+            //textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
         ),
       ),
